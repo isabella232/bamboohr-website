bf9ddf01ff5c9e3356d67db55664742e408eb39b
David Nuescheler <david@nuescheler.ch>
2022-03-06 08:35:43 -0700

    feat: social buttons

--- blocks/article-header/article-header.js
+++ blocks/article-header/article-header.js
@@ -6,6 +6,35 @@ function applyClasses(styles, elements, prefix) {
   });
 }
 
+function createSearch() {
+  const div = document.createElement('div');
+  div.className = 'article-header-search';
+  div.innerHTML = '<a href="/blog/search"><img src="/icons/search.svg" class="icon icon-search"></a>';
+  return (div);
+}
+
+function createSharing() {
+  const { title } = document;
+  const url = window.location.href;
+  const shares = [
+    { icon: 'facebook', url: `https://www.facebook.com/sharer/sharer.php?u=${url}` },
+    { icon: 'twitter', url: `http://twitter.com/share?&url=${url}` },
+    { icon: 'linkedin', url: `https://www.linkedin.com/sharing/share-offsite/?url=${url}` },
+    { icon: 'email', url: `mailto:?subject=${title}&body=${url}` },
+    { icon: 'rss', url: '#' },
+  ];
+  const div = document.createElement('div');
+  div.className = 'article-header-share';
+  shares.forEach((button) => {
+    const a = document.createElement('a');
+    a.href = button.url;
+    a.target = '_blank';
+    a.innerHTML = `<img src="/icons/${button.icon}.svg" class="icon icon-${button.icon}">`;
+    div.append(a);
+  });
+  return (div);
+}
+
 export default async function decorateArticleHeader($block, blockName) {
   applyClasses(['image', 'eyebrow', 'title', 'author-pub'], $block.children, blockName);
   applyClasses(['category', 'read-time'], $block.querySelector('.article-header-eyebrow').firstChild.children, blockName);
@@ -23,4 +52,10 @@ export default async function decorateArticleHeader($block, blockName) {
   // format date
   const $pubdate = $block.querySelector(`.${blockName}-publication-date`);
   $pubdate.textContent = formatDate($pubdate.textContent);
+
+  // sharing
+  $block.append(createSharing());
+
+  // sharing
+  $block.append(createSearch());
 }
